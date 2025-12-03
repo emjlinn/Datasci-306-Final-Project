5f38335991047d0cef11211266cf4656f23e6172
jravi <emjlin@umich.edu>
2025-12-03 21:54:50 +0000

    change heading to app

--- MLapp/app.R
+++ MLapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Group 28: ML App"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
